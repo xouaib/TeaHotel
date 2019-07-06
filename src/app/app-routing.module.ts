97a6fe6fb3f4312c81494e0c36492501e7c6c417
xouaib <gdasoft18@gmail.com>
2019-07-06 12:36:14 +0100

    configure routing using `ng generate module app-routing --flat --module=app`

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { OffersComponent } from './offers/offers.component';
+import { WiddingsComponent } from './widdings/widdings.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/accomodation', pathMatch: 'full' },
+  { path: 'accomodation', component: AccomodationComponent },
+  { path: 'celebrations', component: CelebrationsComponent },
+  { path: 'food-and-beverage', component: FoodAndBeverageComponent },
+  { path: 'lifestyle', component: LifestyleComponent },
+  { path: 'offers', component: OffersComponent },
+  { path: 'widdings', component: WiddingsComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  // redirect to accomodation if the user put wrong url
+  { path: '**', redirectTo: '/accomodation', pathMatch: 'full' },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
